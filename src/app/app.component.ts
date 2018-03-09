0bbe088514633fe87de455fdec7958cc081cbcaa
Helmer salas <yonnich@gmail.com>
2018-03-08 21:31:09 -0600

    Aplicacion en Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './Component/body.component.html',
+})
+export class AppComponent {
+  title = 'app';
+}
